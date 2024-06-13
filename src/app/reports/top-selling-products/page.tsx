539475a875cb47df3702b15b3b76d7edff12caaf
Derick-Almeida <d.a.s2000silva14@gmail.com>
2024-06-13 09:35:10 -0300

    cleaning order page and created top selling products page

--- src/app/reports/top-selling-products/page.tsx
+++ src/app/reports/top-selling-products/page.tsx
@@ -1,5 +1,60 @@
 "use client";
 
+import Filters from "@/components/filters";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { formatPrice } from "@/lib/utils";
+
+const data = [
+  {
+    id: "dsdsqwdsqw",
+    product: "casaco",
+    quantityOrdered: "5",
+    orderValue: "12432",
+  },
+  {
+    id: "dh23ghdsh",
+    product: "calça",
+    quantityOrdered: "2",
+    orderValue: "1235433",
+  },
+];
+
 export default function TopSellingProducts() {
-  return <div>componente produtos mais vendidos</div>;
+  return (
+    <div className="mx-5 sm:mx-12 mt-6">
+      <div className="w-full md:w-[70vw]">
+        <Filters />
+
+        <Table className="mt-8 mb-16">
+          <TableHeader className="bg-secondary/40">
+            <TableRow>
+              <TableHead className="w-1/6 text-left">Produto</TableHead>
+              <TableHead className="w-1/6 text-center hidden xs:table-cell">Qtd. pedidos</TableHead>
+              <TableHead className="w-1/6 text-right ">Vlr. pedidos</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell className="w-1/6 text-left">{item.product}</TableCell>
+
+                <TableCell className="w-1/6 text-center hidden xs:table-cell">
+                  {item.quantityOrdered}
+                </TableCell>
+
+                <TableCell className="w-1/6 text-right">{formatPrice(item.orderValue)}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
 }
