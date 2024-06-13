40fd2e5b5451ad3041a32552e4715e9cb891e00f
Derick-Almeida <d.a.s2000silva14@gmail.com>
2024-06-13 09:16:13 -0300

    connections page

---
+++ src/app/tools/connections/components/form-conection.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Form } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { FormInputField } from "@/components/form-fileds/form-input-field";
+import { formSellerSchema } from "@/schemas/store";
+import { DataType } from "../page";
+
+interface Props {
+  currentData: DataType[number];
+}
+
+export function FormConection({ currentData }: Props) {
+  const form = useForm<z.infer<typeof formSellerSchema>>({
+    resolver: zodResolver(formSellerSchema),
+  });
+
+  const onSubmit = (values: z.infer<typeof formSellerSchema>) => console.log(values);
+
+  return (
+    <div className="pb-12">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormInputField
+            formData={form}
+            name="name"
+            title="Nome"
+            disabled
+            placeholder="exemplo-1718280450"
+            value="exemplo-1718280450"
+          />
+
+          <Button type="submit" className="w-full h-12 save-form-button text-xl">
+            Salvar
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
---
+++ src/app/tools/connections/components/list-conections.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import TableCellActions from "@/components/table-cell-actions";
+import { DataType } from "../page";
+import { cn } from "@/lib/utils";
+
+interface Props {
+  data: DataType;
+  setCurrentTab: (e: string) => void;
+  setCurrentData: (e: DataType[number]) => void;
+  setEditData: (e: boolean) => void;
+}
+
+export function ListConections({ data, setEditData, setCurrentData, setCurrentTab }: Props) {
+  const edit = (itemData: DataType[number]) => {
+    setCurrentTab("add");
+    setCurrentData(itemData);
+    setEditData(true);
+  };
+
+  const remove = () => console.log("remove cell");
+
+  return (
+    <Table className="mt-8 mb-16">
+      <TableHeader className="bg-secondary/40">
+        <TableRow>
+          <TableHead className="w-1/6 whitespace-nowrap text-left">Nome</TableHead>
+          <TableHead className="w-1/6 whitespace-nowrap text-center hidden xsm:table-cell">
+            Status
+          </TableHead>
+          <TableHead className="w-1/6 whitespace-nowrap text-center hidden sm:table-cell">
+            Sessão
+          </TableHead>
+          <TableHead className="w-1/6 text-right">Ação</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data.map((item) => (
+          <TableRow key={item.id}>
+            <TableCell className="w-1/6 text-left">{item.name}</TableCell>
+
+            <TableCell className="w-1/6 text-center hidden xsm:table-cell font-medium">
+              <span
+                className={cn(
+                  "border p-1.5 border-red-900 bg-red-300 text-red-950 font-bold rounded-lg",
+                  item.status === "ativo" && "border-green-900 bg-green-300 text-green-950"
+                )}
+              >
+                {item.status}
+              </span>
+            </TableCell>
+
+            <TableCell className="w-1/6 text-center hidden sm:table-cell">{item.section}</TableCell>
+
+            <TableCellActions
+              className="w-1/6 text-right"
+              editData={() => edit(item)}
+              exclude={remove}
+            />
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
